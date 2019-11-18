f80fe852d56aa860904e45fc4b9bb68b15a2d4f8
Nicolai Roovers <nroovers@gmail.com>
2019-11-18 13:35:52 +0200

    exercise4.4

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
     return 1;
 }
 
+const totalLikes = (blogs) =>{
+    if(blogs && blogs.length >0 )
+    {
+        const likesReducer = (accumulator, blog) => accumulator + blog.likes;
+        return blogs.reduce(likesReducer, 0)
+    }
+    return 0
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
