40360a998f69b84b2da8e7f65abb23c656f40224
Nicolai Roovers <nroovers@gmail.com>
2019-11-28 11:19:41 +0200

    exercise5.14

--- bloglist-frontend/src/components/SimpleBlog.test.js
+++ bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable indent */
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import SimpleBlog from './SimpleBlog'
 
 test('renders content', () => {
@@ -15,6 +15,28 @@ test('renders content', () => {
         <SimpleBlog blog={blog} />
     )
 
+    // component.debug()
+
     expect(component.container).toHaveTextContent('blog title Nicolai')
     expect(component.container).toHaveTextContent('blog has 5 likes')
+})
+
+test('fires button click event', () => {
+    const blog = {
+        title: 'blog title',
+        author: 'Nicolai',
+        likes: 5
+    }
+
+    const mockHandler = jest.fn()
+
+    const component = render(
+        <SimpleBlog blog={blog} onClick={mockHandler}/>
+    )
+  
+    const button = component.getByText('like')
+    fireEvent.click(button)
+    fireEvent.click(button)
+  
+    expect(mockHandler.mock.calls.length).toBe(2)
 })
